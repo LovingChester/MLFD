ea2830be4b6b303430dbe5e462416936e940ae9f
LovingChester <chelsea000719@gmail.com>
2021-09-04 00:18:12 -0400

    ai

--- homework/hw1.py
+++ homework/hw1.py
@@ -1,1 +1,12 @@
+'''
+This program will implement the PLA as states on
+the problem 1.4
+@author Houhua Li
+'''
+import numpy as np
+import matplotlib as plt
+
 print("hello")
+a = np.array([1,2,3,4])
+
+print(a)
