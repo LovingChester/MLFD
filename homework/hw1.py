acd8f2ffaa5e3fe0c920bd373d39c82fc1146a78
LovingChester <chelsea000719@gmail.com>
2021-09-04 01:04:28 -0400

    plot

--- homework/hw1.py
+++ homework/hw1.py
@@ -4,9 +4,22 @@ the problem 1.4
 @author Houhua Li
 '''
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 print("hello")
 a = np.array([1,2,3,4])
-
 print(a)
+
+'''
+plot the f: x1 + x2 + 2 = 0
+'''
+plt.axis([-4, 4, -4, 4])
+plt.xlabel("x1")
+plt.ylabel("x2")
+
+x1 = np.arange(-4, 5)
+x2 = np.array(-x1+2)
+plt.plot(x1,x2)
+
+plt.grid(True)
+plt.show()
