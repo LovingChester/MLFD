08306df9bc5c2e290dcc37334dd5c6922159e632
LovingChester <chelsea000719@gmail.com>
2021-09-04 14:51:07 -0400

    show

--- homework/hw1.py
+++ homework/hw1.py
@@ -7,19 +7,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print("hello")
-a = np.array([1,2,3,4])
-print(a)
 
 '''
 plot the f: x1 + x2 + 2 = 0
 '''
-plt.axis([-4, 4, -4, 4])
+plt.axis([-10, 10, -10, 10])
 plt.xlabel("x1")
 plt.ylabel("x2")
 
-x1 = np.arange(-4, 5)
+x1 = np.arange(-10, 11)
 x2 = np.array(-x1+2)
 plt.plot(x1,x2)
 
 plt.grid(True)
+
+np.random.seed(12)
+D = np.random.randint(-9, 10, size=(20,2))
+print(np.transpose(D))
+plt.plot(np.transpose(D)[0],np.transpose(D)[1], 'ro')
+
 plt.show()
+
+# np.random.seed(20)
+# D = np.random.randint(-4, 5, size=(20,2))
+# print(D)
