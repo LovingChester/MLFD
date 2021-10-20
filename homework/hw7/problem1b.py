749738a695a30ce67f507abc3b3704334625ca73
LovingChester <chelsea000719@gmail.com>
2021-10-19 23:04:20 -0400

    hw7

--- homework/hw7/problem1b.py
+++ homework/hw7/problem1b.py
@@ -8,3 +8,8 @@ from problem1a import *
 Dx, Dy, final_w, count = gather_data("ZipDigits.train")
 
 E_in = np.linalg.norm(np.matmul(Dx, final_w) - Dy) ** 2 / count
+print("E_in is: {}".format(E_in))
+
+Dx, Dy, final_w, count = gather_data("ZipDigits.test")
+E_test = np.linalg.norm(np.matmul(Dx, final_w) - Dy) ** 2 / count
+print("E_test is: {}".format(E_test))
