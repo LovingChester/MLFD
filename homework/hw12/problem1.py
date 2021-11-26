ccd4b0a014eab8dd8e07bb623c627a3cc853b9f1
LovingChester <chelsea000719@gmail.com>
2021-11-26 01:55:34 -0500

    hw11

--- homework/hw12/problem1.py
+++ homework/hw12/problem1.py
@@ -3,6 +3,8 @@ import numpy as np
 def MLP_training(Dx, Dy, W_h, W_o, trans):
     row, col = np.size(Dx, 0), np.size(Dx, 1)
     t = 0
+    g_o = 0
+    g_h = 0
     G_o = 0 * W_o
     G_h = 0 * W_h
     while t < 1:
@@ -17,32 +19,35 @@ def MLP_training(Dx, Dy, W_h, W_o, trans):
             elif trans == "sign":
                 o = np.sign(np.matmul(np.transpose(W_o), s))
             
-        # backward propagation
-        sens_o = 0
-        if trans == "identity":
-            sens_o = (1 / (2*row)) * (o - Dy)
-        elif trans == "tanh":
-            sens_o = (np.ones((np.size(o, 0), 1)) - o * o) * \
-                (1 / (2*row)) * (o - Dy)
-        elif trans == 'sign':
-            sens_o = 0 * (1 / (2*row)) * (o - Dy)
+            # backward propagation
+            sens_o = 0
+            if trans == "identity":
+                sens_o = 2 * (o - Dy)
+            elif trans == "tanh":
+                sens_o = (1 - o * o) * 2 * (o - Dy)
+            elif trans == 'sign':
+                sens_o = 0 * 2 * (o - Dy)
         
-        sens_h = (np.ones((np.size(s, 0), 1)) - s * s) * np.matmul(W_o, sens_o)
+            sens_h = (1 - s * s) * np.matmul(W_o, sens_o)
 
-        g_o = np.outer(s, np.transpose(sens_o))
-        g_h = np.outer(np.transpose(Dx[[i], :]), np.transpose(sens_h))
-        G_o += g_o
-        G_h += g_h
+            g_o = np.outer(s, np.transpose(sens_o))
+            g_h = np.outer(np.transpose(Dx[[i], :]), np.transpose(sens_h))
+            G_o += (1 / (4*row)) * g_o
+            G_h += (1 / (4*row)) * g_h
 
         t += 1
 
-    return G_o, G_h
+    return g_o, g_h, G_o, G_h
 
-Dx = np.array([[1, 2]])
-Dy = np.array([[1]])
-W_h = np.array([[0.25, 0.25], [0.25, 0.25]])
-W_o = np.array([[0.25], [0.25]])
 
-G_o, G_h = MLP_training(Dx, Dy, W_h, W_o, "tanh")
-print(G_o)
-print(G_h)
+if __name__ == '__main__':
+    Dx = np.array([[1, 2]])
+    Dy = np.array([[1]])
+    W_h = np.array([[0.25, 0.25], [0.25, 0.25]])
+    W_o = np.array([[0.25], [0.25]])
+
+    g_o, g_h, G_o, G_h = MLP_training(Dx, Dy, W_h, W_o, "tanh")
+    print(g_o)
+    print(g_h)
+    print(G_o)
+    print(G_h)
