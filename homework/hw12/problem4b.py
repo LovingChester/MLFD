cf83f83ea787aef5cb4c9d5c02785e25f60dd069
LovingChester <chelsea000719@gmail.com>
2021-12-02 17:47:04 -0500

    hw12

---
+++ homework/hw12/problem4b.py
@@ -0,0 +1,10 @@
+from problem4a import *
+
+def compute_E_cv(Dx, Dy):
+    
+    return
+
+if __name__ == '__main__':
+    Dx_train, Dy_train, Dx_test, Dy_test = gather_data(["ZipDigits.train", "ZipDigits.test"])
+
+    print(get_kernel(Dx_train))
